a019195556fcd18c0604e47161d420d59d2a9d21
dennis chamba <dennis.chamba@epn.edu.ec>
2023-05-25 10:46:50 -0500

    Cambios en el archivo clases

--- Leccion_Kotlin/src/main/kotlin/clases.kt
+++ Leccion_Kotlin/src/main/kotlin/clases.kt
@@ -1,4 +1,4 @@
-fun main(arg: Array<String>){
+
     abstract class NumerosJava{
         protected val numeroUno: Int
         private val numeroDos: Int
@@ -54,21 +54,46 @@ fun main(arg: Array<String>){
         dos:Int?//Parametros
         ):this(
         uno,
-        if(dos ==null)0 else dos,
+        if(dos == null)0 else dos,
         )
 
         constructor( //Segundo constructor
         uno: Int?,//parametros
         dos:Int?//Parametros
         ):this(
-        if(uno ==null)0 else uno,
-        if(dos ==null)0 else dos,
+        if(uno == null)0 else uno,
+        if(dos == null)0 else dos,
         )
-
         public fun sumar(): Int{
             val total = numeroUno + numeroDos
+            agregarHistorial(total) //this.agregarHistorial(total)
             return total
+        }
+        companion object{//atributos y mentodos compartidos singletons o static de esta clase
+        // todas las instancias de esta clase comparten estos atributos y metodos
+        // dentro del companion object
+        val pi = 3.14
+            fun elevarAlCuadrado(num: Int): Int{
+                return num * num
+            }
+            val historialSumas = ArrayList<Int>()
+            fun agregarHistorial(valorNuevaSuma: Int){
+                historialSumas.add(valorNuevaSuma)
+            }
         }
     }
 
-}
+    fun main(arg: Array<String>){
+        val sumaUno = suma(1,1)
+        val sumaDos = suma(null, 1)
+        val sumaTres = suma(1, null)
+        val sumacuatro = suma(null, null)
+        sumaUno.sumar()
+        sumaDos.sumar()
+        sumaTres.sumar()
+        sumacuatro.sumar()
+
+        println(suma.pi)
+        println(suma.elevarAlCuadrado(2))
+        println(suma.historialSumas)
+    }
