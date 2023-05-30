6ca9d500bb93952d7e0ebfc4a9f3ad5ef8ee8895
dennis chamba <dennis.chamba@epn.edu.ec>
2023-05-30 08:12:35 -0500

    operador map y filter

--- Leccion_Kotlin/src/main/kotlin/arreglos.kt
+++ Leccion_Kotlin/src/main/kotlin/arreglos.kt
@@ -24,4 +24,21 @@ fun main(arg: Array<String>){
 
     arreglosEstaticos.forEachIndexed { index:Int, valorActual: Int ->
         println("valor actual: ${valorActual} indice: ${index}") }
+
+    //operador map
+    //crear un nuevo arreglo mutando los elementos del arreglo original
+    val respuestaMap: List<Double> = arreglosDinamicos.map {
+            valorActual: Int -> return@map valorActual.toDouble() + 100.00 }
+    println(respuestaMap)
+
+    //filter
+    //usa una expresion booleana y retorna un nuevo arreglo
+    val resepuestaFilter: List<Int> = arreglosDinamicos.filter { valorActual: Int ->
+        val mayoresACinco: Boolean = valorActual > 5 //expresion condicion
+        return@filter mayoresACinco
+    }
+    val respuestaFilter2 = arreglosDinamicos.filter { it <= 5 }
+    println(resepuestaFilter)
+    println(respuestaFilter2)
+
 }
