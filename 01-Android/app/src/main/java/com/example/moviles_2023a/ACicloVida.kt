b001759c11354634481979b5f0f01079aa697179
dennis chamba <dennis.chamba@epn.edu.ec>
2023-06-08 09:56:54 -0500

    aplicativo en ciclo de vida

---
+++ 01-Android/app/src/main/java/com/example/moviles_2023a/ACicloVida.kt
@@ -0,0 +1,18 @@
+package com.example.moviles_2023a
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ACicloVida : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aciclo_vida)
+    }
+
+    fun irActividad( clase: Class<*>){
+        val intent = Intent(this, clase)
+        startActivity(intent)
+        // this.starActivity()
+    }
+}
