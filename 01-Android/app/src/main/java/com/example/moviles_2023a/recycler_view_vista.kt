35f9a7a0561b145c4ba65617e4e7345b812721c3
dennis chamba <dennis.chamba@epn.edu.ec>
2023-07-14 23:02:06 -0500

    error

---
+++ 01-Android/app/src/main/java/com/example/moviles_2023a/recycler_view_vista.kt
@@ -0,0 +1,11 @@
+package com.example.moviles_2023a
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recycler_view_vista : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view_vista)
+    }
+}
