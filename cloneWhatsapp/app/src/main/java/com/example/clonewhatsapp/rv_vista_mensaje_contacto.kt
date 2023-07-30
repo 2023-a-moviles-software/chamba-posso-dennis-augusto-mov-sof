9fe685f4ae444cb42ed5b544327378da7ace5a28
dennis chamba <dennis.chamba@epn.edu.ec>
2023-07-29 19:30:54 -0500

    Vista mensajes

---
+++ cloneWhatsapp/app/src/main/java/com/example/clonewhatsapp/rv_vista_mensaje_contacto.kt
@@ -0,0 +1,11 @@
+package com.example.clonewhatsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class rv_vista_mensaje_contacto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rv_vista_mensaje_contacto)
+    }
+}
