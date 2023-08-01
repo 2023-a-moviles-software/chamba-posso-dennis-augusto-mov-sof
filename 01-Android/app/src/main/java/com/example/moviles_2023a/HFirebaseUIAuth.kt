ed2d473ea47ffbc516612092ac0b210c7df6e920
dennis chamba <dennis.chamba@epn.edu.ec>
2023-08-01 15:50:50 -0500

    autenticacion con fireestore

--- 01-Android/app/src/main/java/com/example/moviles_2023a/HFirebaseUIAuth.kt
+++ 01-Android/app/src/main/java/com/example/moviles_2023a/HFirebaseUIAuth.kt
@@ -4,15 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.IdpResponse
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 
 class HFirebaseUIAuth : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hfirebase_uiauth)
-
+        val btnLogin = findViewById<Button>(R.id.btn_login_firebase)
+        btnLogin.setOnClickListener{
+            val providers = arrayListOf(
+            //arreglo de providers para logearse
+            AuthUI.IdpConfig.EmailBuilder().build()
+            )
+            //construimos el intent de login
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build()
+            //respuesta del intent de login
+            respuestaLoginAuthUI.launch(signInIntent)
+        }
+        val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+        btnLogout.setOnClickListener{seDeslogeo()}
 
     }
 
@@ -36,5 +53,13 @@ class HFirebaseUIAuth : AppCompatActivity() {
         if(res.isNewUser == true){
             //registrarUsuarioPorPrimeraVez(res)
         }
+    }
+
+    fun seDeslogeo(){
+        val btnLogin = findViewById<Button>(R.id.btn_login_firebase)
+        val btnLogout = findViewById<Button>(R.id.btn_logout_firebase)
+        btnLogout.visibility = View.INVISIBLE
+        btnLogin.visibility = View.VISIBLE
+        FirebaseAuth.getInstance().signOut()
     }
 }
